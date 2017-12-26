cee78eed211cd7c5e538cfa1e980e43a98fda169
reivax-boucoi <reivax.boucoi@laposte.net>
2017-12-26 18:52:41 +0100

    Add files via upload

--- P5JS/sudoku/sketch.js
+++ P5JS/sudoku/sketch.js
@@ -1,8 +1,8 @@
-var puzzle = "6.......7....9..2.3.1..259.8....7.13....8....76.3....8.782..1.6.5..3....2.......9";
-//var puzzle = "2..1.5..3.54...71..1.2.3.8.6.28.73.4.........1.53.98.6.2.7.1.6..81...24.7..4.2..1";
-//var puzzle = "....6...8......53...2593.4...1.7.3..9.31.56.4..4.3.9...2.3568...98......6...8....";
+//var puzzle = "6.......7....9..2.3.1..259.8....7.13....8....76.3....8.782..1.6.5..3....2.......9";
+//var puzzle = "2..1.5..3.54...71..1.2.3.8.6.28.73.4.........1.53.98.6.2.7.1.6..81...24.7..4.2..1"; // solved !
+//var puzzle = "....6...8......53...2593.4...1.7.3..9.31.56.4..4.3.9...2.3568...98......6...8...."; // solved !
 //var puzzle = ".7.456......7.....4.62..7.95378416928..6923752693754187.3...2.4.....4..7...527...";
-//var puzzle = "000023000004000100050084090001070902093006000000010760000000000800000004060000587";
+var puzzle = "....23.....4...1...5..84.9...1.7.9.2.93..6.......1.76..........8.......4.6....587";
 //var puzzle = "050007690000040000009000000000100004000230008008000150000400003006080209002005000";
 var grid = [];
 var current;
@@ -10,6 +10,8 @@ var step;
 var solve;
 var debug;
 var stuck;
+var cnt;
+
 var BoxType = function () {
 	this.nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
@@ -43,12 +45,12 @@ function drawGrid() {
 					rect(i * spacing + 5, j * spacing + 5, spacing - 10, spacing - 10);
 				}
 				fill(0);
-				text(grid[i + j * 9].nums[0], i * spacing + 20, j * spacing + 25);
+				text(grid[i + j * 9].nums[0], i * spacing + spacing*.45, j * spacing + spacing*.6);
 			} else {
 				if (debug.checked()) {
 					for (var x = 0; x < grid[i + j * 9].nums.length; x++) {
 						fill(0);
-						text(grid[i + j * 9].nums[x], i * spacing + ((x % 3) * spacing / 4) + 5, j * spacing + (floor(x / 3) * spacing / 4) + 15);
+						text(grid[i + j * 9].nums[x], i * spacing + ((x % 3) * spacing / 4) + spacing*.15, j * spacing + (floor(x / 3) * spacing / 4)+ spacing*.35);
 					}
 				}
 			}
@@ -67,8 +69,6 @@ function solveGrid() {
 		console.log("hum...");
 		stuck = 0;
 		var trueGrid = grid;
-
-
 	}
 }
 
@@ -84,12 +84,31 @@ function checkFinished() {
 	return finished;
 }
 
-function checkRow(i) {
+function checkNeighbors(index,x){
+	for(var i = 1; i < 10; i++){
+		if (grid[index].nums.length == 1 && index != x) {
+			if((grid[x].nums.indexOf(i)!=-1) && grid[index].nums[0]==i){
+				grid[x].nums.splice(grid[x].nums.indexOf(i),1);
+			}
+		}
+		if(grid[index].nums.indexOf(i)!=-1){
+			cnt[i-1]++;
+		}
+	}
+}
+
+function checkRow(x) {
+	cnt=[0,0,0,0,0,0,0,0,0];
 	for (var j = 0; j < 9; j++) {
-		if (grid[floor(i / 9) * 9 + j].nums.length == 1 && floor(i / 9) * 9 + j != i) {
-			for (var x = 0; x < grid[i].nums.length; x++) {
-				if (grid[floor(i / 9) * 9 + j].nums[0] == grid[i].nums[x]) {
-					grid[i].nums.splice(x, 1);
+		var index = floor(x / 9) * 9 + j;
+		checkNeighbors(index,x);
+	}
+	for(var i=0;i<9;i++){
+	if(cnt[i]==1){
+		for (var j = 0; j < 9; j++) {
+				var index = floor(x / 9) * 9 + j;
+				if(grid[index].nums.indexOf(i+1)!=-1){
+					setBox(index,i+1);
 				}
 			}
 		}
@@ -97,34 +116,29 @@ function checkRow(i) {
 }
 
 function checkCol(x) {
-	
-	var cnt=[0,0,0,0,0,0,0,0,0];
+	cnt=[0,0,0,0,0,0,0,0,0];
 	for (var j = 0; j < 9; j++) {
 		var index = (x % 9) + j * 9;
-		if (grid[index].nums.length == 1 && index != x) {
-			var i = grid[index].nums[0];
-			if (grid[x].nums.indexOf(i)!=-1) {
-				grid[x].nums.splice(grid[x].nums.indexOf(i), 1);
+		checkNeighbors(index,x);
+	}
+	for(var i=0;i<9;i++){
+	if(cnt[i]==1){
+		for (var j = 0; j < 9; j++) {
+				var index = (x % 9) + j * 9;
+				if(grid[index].nums.indexOf(i+1)!=-1){
+					setBox(index,i+1);
+				}
 			}
 		}
 	}
 }
 
 function checkSquare(x) {
-	var cnt=[0,0,0,0,0,0,0,0,0];
+	cnt=[0,0,0,0,0,0,0,0,0];
 	for (var j = 0; j < 3; j++) {
 		for (var k = 0; k < 3; k++) {
 			var index = 3 * floor((x % 9) / 3) + 27 * floor(x / 27) + j + k * 9;
-			for(var i=1;i<10;i++){
-				if (grid[index].nums.length == 1 && index != x) {
-					if((grid[x].nums.indexOf(i)!=-1) && grid[index].nums[0]==i){
-						grid[x].nums.splice(grid[x].nums.indexOf(i),1);
-					}
-				}
-				if(grid[index].nums.indexOf(i)!=-1){
-					cnt[i-1]++;
-				}
-			}
+			checkNeighbors(index,x,i);
 		}
 	}
 	for(var i=0;i<9;i++){
@@ -181,15 +195,16 @@ function populateGrid() {
 }
 
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(windowHeight*.9, windowHeight*.9);
 	background(200);
+	textSize(18);
 	step = createButton("step");
 	step.mousePressed(simplify);
 
 	solve = createButton("solve!");
 	solve.mousePressed(solveGrid);
 
-	debug = createCheckbox("Enable debugging", true);
+	debug = createCheckbox("Enable debugging", false);
 	debug.changed(drawGrid);
 
 	current = 0;
