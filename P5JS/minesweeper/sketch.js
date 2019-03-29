2a4856710f50650525ad6655cc9744f6016d8c30
reivax-boucoi <reivax.boucoi@laposte.net>
2019-03-29 11:11:23 +0100

    a

--- P5JS/minesweeper/sketch.js
+++ P5JS/minesweeper/sketch.js
@@ -1,28 +1,9 @@
-var acolor = 0;
-var b;
-var ps;
+var w=20;
 
 function setup() {
 	createCanvas(400, 400);
-	createP(random(100));
-	b = createButton("Click Me !");
-	b.mouseOver(ButtonPress);
-	ps = select('#MyId');
 }
 
 function draw() {
-	background(255 - acolor);
-	fill(acolor);
-	ellipse(200, 200, 100, 100);
-	acolor = (acolor + 1) % 255;
-	ps.style('background', '#F0F');
-	//console.log(frameCount +"  "+acolor);
-}
-
-function MousePressed() {
-	createP(random(100));
-}
-
-function ButtonPress() {
-	createP(random(100));
+	background(255);
 }
