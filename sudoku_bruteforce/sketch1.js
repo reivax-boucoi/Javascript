5633f0d69e3a67773e2df4f096ac9b073d15bf52
reivax-boucoi <reivax.boucoi@laposte.net>
2019-05-04 00:40:26 +0200

    a

--- sudoku_bruteforce/sketch1.js
+++ sudoku_bruteforce/sketch1.js
@@ -99,12 +99,8 @@ function simplifyBox(g,index){
         l=getKnown(getLine(g,g[index].line));
         c=getKnown(getCol(g,g[index].col));
         s=getKnown(getSquare(g,g[index].square));
-        console.log(l);
-        console.log(c);
-        console.log(s);
-        l.concat(c);
-        l.concat(s);
-        console.log(l);
+        l=l.concat(c);
+        l=l.concat(s);
         for(var n=0;n<l.length;n++){
             i=g[index].nums.indexOf(l[n]);
             if(i>0){
