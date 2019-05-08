b041e20584ed8cf0c94208fb3f539cb34bd76e7b
reivax-boucoi <reivax.boucoi@laposte.net>
2019-05-08 12:36:54 +0200

    a

--- graph_dij/Graph.js
+++ graph_dij/Graph.js
@@ -1,18 +1,32 @@
-function Node(x,y){
+function Node(x,y,nb){
     this.x=x;
     this.y=y;
-    this.visited=false;
+    this.nb=nb
+    this.visited=0;//0: normal, 1:start, 2:end, 3:visited
     this.currentweight=Infinity;
     this.paths=[];
-    this.show=function(){
-        if(this.visited){
+    this.show=function(c){
+        
+        if(this.visited==2){
+            fill(200,0,0);
+        }else if (this.visited==1){
+            fill(0,200,0);
+        }else if(this.visited==3){
             fill(255,0,128);
         }else{
             fill(255,0,255);
         }
-        strokeWeight(3);
-        stroke(255);
-        circle(this.x,this.y,40)
+        strokeWeight(4);
+        if(this==c){
+            stroke(255,0,0);
+        }else{
+            stroke(255);   
+        }
+        circle(this.x,this.y,40);
+        noStroke();
+        fill(255);
+        textSize(30);
+        text(this.nb,this.x,this.y+3);
     }
     this.addPath=function(n,w){
         this.paths.push(new Path(this,n,w));
--- graph_dij/sketch1.js
+++ graph_dij/sketch1.js
@@ -1,31 +1,54 @@
 var N=[];
 let json;
+
+var start;
+var current;
+var end;
+
 function preload(){
     json=loadJSON("Nodes.json");
 }
 function showPaths(){
     for(var n=0;n<json.Paths.length;n++){
         p=json.Paths[n];
-        stroke(255);
-        strokeWeight(10/p.w);
+        stroke(220);
+        strokeWeight(15/sqrt(p.w));
         line(N[p.n1].x,N[p.n1].y,N[p.n2].x,N[p.n2].y);
+        stroke(0);
+        strokeWeight(2);
+        fill(255);
+        textSize(20);
+        text(p.w,(N[p.n1].x+N[p.n2].x)/2,(N[p.n1].y+N[p.n2].y)/2);
     }
 }
 function setup(){
-    createCanvas(windowHeight*.9, windowHeight*.9);
-    
+    cnv=createCanvas(windowHeight*.9, windowHeight*.9);
+    cnv.mousePressed(click);
+    textAlign(CENTER,CENTER);
     for(var n=0;n<json.Nodes.length;n++){
-        N.push(new Node(json.Nodes[n].x,json.Nodes[n].y));
+        N.push(new Node(json.Nodes[n].x,json.Nodes[n].y,n));
+        if(json.Nodes[n].start){
+            N[N.length-1].visited=1;
+            current=N[N.length-1];
+        }
+        if(json.Nodes[n].end){
+            N[N.length-1].visited=2;
+            end=N[N.length-1];
+        }
     }
     for(var n=0;n<json.Paths.length;n++){
         p=json.Paths[n];
         N[p.n1].addPath(N[p.n2],p.w);
     }
 }
+function click(){
+    console.log(current.paths);
+}
+
 function draw(){
     background(0);
     showPaths();
     for(var n=0;n<N.length;n++){
-        N[n].show();
+        N[n].show(current);
     }
 }
