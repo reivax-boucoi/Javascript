89a602f46f67e0879692673210d1f47ace89a66e
flo <flo@test.fr>
2019-07-09 15:31:36 +0200

    a

--- Terrain/NoiseMap/sketch/Terrains.js
+++ Terrain/NoiseMap/sketch/Terrains.js
@@ -1,11 +1,11 @@
 class Terrains {
 	
   constructor() {
-    terrain[0]=new Terrain("sea",.3,#07528E);
-    terrain[1]=new Terrain("sand",.35,#FBD48A);
-    terrain[2]=new Terrain("earth",.6,#627E0F);
-    terrain[3]=new Terrain("hill",.85,#4F4E3C);
-    terrain[4]=new Terrain("peak",1,#FEF9F5);
+    terrain[0]=new Terrain("sea",.3,color('#07528E'));
+    terrain[1]=new Terrain("sand",.35,color('#FBD48A'));
+    terrain[2]=new Terrain("earth",.6,color('#627E0F'));
+    terrain[3]=new Terrain("hill",.85,color('#4F4E3C'));
+    terrain[4]=new Terrain("peak",1,color('#FEF9F5'));
     
   }
 }
