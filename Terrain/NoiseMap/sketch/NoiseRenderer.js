1eee21810625dd3d016baf4e6a713307196ac7f8
reivax-boucoi <reivax.boucoi@laposte.net>
2019-07-10 18:56:07 +0200

    a

--- Terrain/NoiseMap/sketch/NoiseRenderer.js
+++ Terrain/NoiseMap/sketch/NoiseRenderer.js
@@ -9,16 +9,14 @@ class NoiseRenderer {
 		this.nmap.generate();
 	}
 	render() {
-		
 		background(0);
-		loadPixels();
 		for (var y=0; y<this.nmap.n[0].length; y++) {
 		  for (var x=0; x<this.nmap.n.length; x++) {
 			var c;
 			switch(this.renderMode){
 			 case 1:
 			   for(var i=0;i<this.terrains.terrain.length;i++){
-				 if(this.terrains.terrain[i].level>=map(this.nmap.n[x][y],this.nmap.minNoiseHeight,this.nmap.maxNoiseHeight,0,1)){
+				 if(this.terrains.terrain[i].level>=map(this.nmap.n[x][y],this.nmap.minNoiseHeight,this.nmap.maxNoiseHeight,1,0)){
 				   c=this.terrains.terrain[i].col;
 				   break;
 				 }
@@ -28,9 +26,9 @@ class NoiseRenderer {
 			  c=map(this.nmap.n[x][y],this.nmap.minNoiseHeight,this.nmap.maxNoiseHeight,0,255);
 			 break;
 			}
-			pixels[x*this.nmap.w+y]=c;
+			stroke(c);
+            point(x,y);
 		  }
 		}
-		updatePixels();
 	}
 }
--- Terrain/NoiseMap/sketch/sketch.js
+++ Terrain/NoiseMap/sketch/sketch.js
@@ -3,18 +3,27 @@ var nm;
 var nr;
 
 function setup() {
-  createCanvas(640, 480);
-  nm=new NoiseMap(width, height, 100, 4, 2, 0.5);
-  nr=new NoiseRenderer(nm);
-  background(0);
-  nr.render();
+    createCanvas(640, 480);
+    nm=new NoiseMap(width, height, 100, 4, 2, 0.5);
+    nr=new NoiseRenderer(nm);
+    scaleSlider = createSlider(10, 10000, 100);
+    scaleSlider.position(25, 25);
+    updateButton = createButton('Generate');
+    updateButton.position(25, 25);
+    updateButton.mousePressed(button_update); 
+    background(0);
+    nr.render();
 }
 
 function draw() {
-	
+    
 }
 
 function mouseReleased(){
-	nr.renderMode=1-nr.renderMode;
-	nr.render();
+    nr.renderMode=1-nr.renderMode;
+    nr.render();
+}
+function button_update(){
+    
+    
 }
