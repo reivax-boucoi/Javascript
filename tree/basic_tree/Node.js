463e4abef1da39d29857a6ca28342a504e85b6f6
reivax-boucoi <reivax.boucoi@laposte.net>
2019-08-26 09:16:43 +0200

    a

---
+++ tree/basic_tree/Node.js
@@ -0,0 +1,20 @@
+function Node(p,value){
+    this.x=width/2;
+    this.y=50;
+    this.parent=p;
+    this.value=value;
+    this.leftChild=null;
+    this.RightChild=null;
+    this.show=function(){
+            fill(0,0,255);
+        strokeWeight(4);
+            stroke(255);   
+        circle(this.x,this.y,40);
+        noStroke();
+        fill(255);
+        textSize(20);
+        text(this.value,this.x,this.y+2);
+    }
+}
+
+
